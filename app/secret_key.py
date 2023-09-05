d4c9ac725f136ed47d6aab92f90ced1422e6d272
BrianKeybet <86772792+BrianKeybet@users.noreply.github.com>
2023-09-05 22:14:19 +0300

    Initial commit

---
+++ app/secret_key.py
@@ -0,0 +1,4 @@
+from decouple import config
+
+# OpenWeatherMap API Key >>> https://home.openweathermap.org/api_keys
+KEY = config('SECRET_WM_API_KEY')
---
+++ app/views.py
@@ -0,0 +1,18 @@
+# Create your views here.
+from django.shortcuts import render
+from .weather import getWeather
+
+# Create your views here.
+
+def index(request):
+     try:
+         if request.method == 'POST':
+             city = request.POST['city']
+             context = getWeather(city)
+             return render(request, 'home.html', context)
+         else:
+             city_weather = {}
+             context = {'city_weather': city_weather}
+             return render(request, 'home.html', context)
+     except:
+        return render(request, 'errors.html')
